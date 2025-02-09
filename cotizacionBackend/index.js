a971843a8b473344768f8bca96f1c2696be6ad83
favio-ballon <favioballon@gmail.com>
2025-02-09 00:04:33 -0400

    edit: webhook

--- cotizacionBackend/index.js
+++ cotizacionBackend/index.js
@@ -4,6 +4,7 @@ var cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const fileUpload = require('express-fileupload')
+const { exec } = require('child_process')
 
 app.use(bodyParser.json())
 app.use(
@@ -13,8 +14,8 @@ app.use(
 )
 
 var corsOptions = {
-    origin: ['https://www.cotizafemco.com', 'https://github.com'], // Allow frontend & GitHub
-};
+    origin: ['https://www.cotizafemco.com', 'https://github.com'] // Allow frontend & GitHub
+}
 
 app.use(cors(corsOptions))
 
@@ -38,27 +39,29 @@ db.sequelize
 require('./routes')(app)
 
 //make static files available
-app.use('/uploads/cotizacion', express.static('uploads/cotizacion'));
+app.use('/uploads/cotizacion', express.static('uploads/cotizacion'))
 
-app.post("/webhook", (req, res) => {
-    console.log("Webhook received from GitHub");
+app.post('/webhook', (req, res) => {
+    console.log('Webhook received from GitHub')
 
-    exec("C:\\Users\\59179\\Documents\\web\\CotizacionFemco\\deploy.bat", (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error: ${error.message}`);
-            return res.status(500).send("Deployment failed");
+    exec(
+        'C:\\Users\\59179\\Documents\\web\\CotizacionFemco\\deploy.bat',
+        (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error: ${error.message}`)
+                return res.status(500).send('Deployment failed')
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`)
+                return res.status(500).send('Deployment error')
+            }
+
+            console.log(`stdout: ${stdout}`)
+            res.status(200).send('Deployment successful')
         }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return res.status(500).send("Deployment error");
-        }
+    )
+})
 
-        console.log(`stdout: ${stdout}`);
-        res.status(200).send("Deployment successful");
-    });
-});
-
-
-app.listen(3000,"0.0.0.0", function () {
+app.listen(3000, '0.0.0.0', function () {
     console.log('Ingrese a http://localhost:3000')
 })
